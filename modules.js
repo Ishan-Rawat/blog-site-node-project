1d00bb3d8d79a2de6c924ed5323bc76f52061672
Ishan-Rawat <rawatishan03@gmail.com>
2023-05-24 23:56:12 +0530

    exporting multiple P1

--- modules.js
+++ modules.js
@@ -1,3 +1,7 @@
 const xyz = require('./peoples.js')
+//Since we are importing an object from the poeples.js file or module, we need to access those lists 
+// in the same way we access properties of objects.
 
 console.log(xyz)
+console.log(xyz.ages)
+console.log(xyz.people)
--- peoples.js
+++ peoples.js
@@ -1,3 +1,17 @@
+//exporting multiple "things"
 const people = ['senku', 'kohaku', 'ginro', 'kinro']
+const ages  = [3100, 16, 15, 15]
+// To export multiple "things" we need to export an object, where the multiple "things" are its properties 
+module.exports = {
+    // people : people ==> here key = people and value = the list people defined above
+    //We usually dont write it this way because if the key value pair have the same identifiers then JS
+    //allows us to mention the identifier only once and it handles the rest by itself.
 
-module.exports = people
+    //so we can export the above lists by just writing:
+    people,ages
+
+    //instead of writing:
+    /* people: people
+       ages: ages
+    */
+}
