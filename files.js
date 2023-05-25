8aa3c65cf0d5f6e3bf4f123710384b9cc8431f46
Ishan-Rawat <rawatishan03@gmail.com>
2023-05-25 20:12:44 +0530

    deleting files

--- files.js
+++ files.js
@@ -3,26 +3,11 @@
 
 const fs = require('fs');
 
-//creating and deleting DIRECTORIES:
-/*The fs.mkdir() function throws an error if the directory to be created already exists. 0
-* We can check if a directory exists by using the fs.existsSync() method. It returns true if it does.
-* we use the not operator (!) to negate the true to false so that we run the mkdir() func when the dir doesnt exist.
-* fs.existsSync() is a synchronous function and it will BLOCK further execution till it is finished executing
-*/
-if(! fs.existsSync('./assets')){
-    fs.mkdir('./assets', (err) => {
-        if(err){
-            console.log(err)
-        }
-        console.log('folder created');
-    });
-} else{
-    //if the directory already exists we will delete it.
-    //We can use the fs.rmdir() method to delete directoris
-    fs.rmdir('./assets', (err) => {
-        if(err){
-            console.log(err)
-        }
-        console.log('folder deleted');
-    });
-}
+//DELETING files
+
+fs.unlink('./docs/deleteMe.txt', (err) => {
+    if(err){
+        console.log(err)
+    }
+    console.log('file deleted');
+})
