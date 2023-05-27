0ec9805629ca991090c12544d81b92f348a3e771
Ishan-Rawat <rawatishan03@gmail.com>
2023-05-27 17:00:44 +0530

    request object

--- server.js
+++ server.js
@@ -2,29 +2,19 @@
 
 const http = require('http');
 
-/*
-* Here we are storing an instance of the server in a variable so that we can use it again for something like
-  web sockets.
-* We give this server function a call back function that is run everytime it receives a request. This step is
-  not necessary.
-* This function has to be given 2 arguments: a request object and a response object.
-* The request object stores information such as request type(GET, POST,etc), request URL, etc.
-*/
 const server = http.createServer((req, res) => {
-    console.log('Request made');
+    console.log(req.url, req.method);
+    //The request object contains a LOT of details. 
+    //So here we are only logging the request url and the request type such as GET and POST (this is given by req.method)
+    /*
+    The output of req.url is the part succeeding the localhost:3000 part.
+    For example, if the request url is http://localhost:3000/about then console.log will return:
+    /about
+    */
 });
 
-//The server won't do anything without invoking the listener function.
+
 server.listen(3000, 'localhost', () => {
     console.log('Listening for requests on port 3000');
 });
 
-/*  LOCALHOST and PORT NUMBER
-*'localhost' is basically a URL that is mapped to a very specific IP address called the loopback address.
-  loopback address = 127.0.0.1. This address just takes us to our own computer.
-
-* PORT NUMBER: its basically a specific channel, gateway or a door that a specific program should communicate through.
-* The are usually many different programs on our PC that communicate with the internet, so port numbers are
-  used to keep the data for specific appliactions separate.
-
-*/
