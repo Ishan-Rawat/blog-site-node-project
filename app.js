1e38a629d26747293f6d0b9dcb5454e7b98abb53
Ishan-Rawat <rawatishan03@gmail.com>
2023-05-30 22:02:09 +0530

    404 response

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-// As our application scales, our previous node server becomes kinda useless. Express is a framework that 
-// allows us to create servers that handle multiple requests, and make routing and stuff easier
-
 const express = require('express');
 
 const app = express();
 
 app.listen(3000); 
 
-//The following code routes a response for the localhost:3000 request URL
-//This time we are sending a HTML response by reading a file
 app.get('/', (req, res) => {
     res.sendFile('./views/index.html', {root: __dirname});
-    //The sendFile() method treats the first argument as an absolute path by default.
-    //Since we have given it a relative path, in the next argument we give it an object that tells it what the current directory is(so that it can resolve the relative path)
 });
 
-//To respond to other request URLs we can jus create more app.get calls(not sure what the correct word is)
 app.get('/about', (req, res) => {
     res.sendFile('./views/about.html', {root: __dirname});
 });
 
-//REDIRECTS
 app.get('/about-us', (req, res) => {
     res.redirect('/about');
-    //This method forces the browser to change the request to /about or something not sure
-    //automatically sets the status code to 301 (checked it on chrome, it was 302)
 });
+
+// To send a default response for invalid URLs, we use the following function:
+// We use the .use() function to create and fire middleware 
+app.use((req, res) => {
+    res.sendFile('./views/404.html', {root: __dirname});
+});
+
+/**How this works is that for every request encountered Node tries to match the request URL with the .get() methods from top to bottom
+ * It then executes the first function whose first arg matches the requested URL
+ * Incase of an invalid URL request, it reaches the app.use() method, and when it does it stops matching and executes app.use()
+ * This is the reason why we must put the app.use() method at the VERY LAST.
+ */
