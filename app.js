1fc883e1b992e911c9f9f0bf3fdd63c37494bfb5
Ishan-Rawat <rawatishan03@gmail.com>
2023-05-31 21:59:46 +0530

    using third party middlewar

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
@@ -6,22 +7,13 @@ app.listen(3000);
 
 app.set('view engine', 'ejs');
 
-//Creating our own custom middleware function
-//The following middleware function just logs out some of the details about the request we have received
-app.use((req, res, next) => {
-    console.log('new request made:');
-    console.log('host: ', req.hostname);
-    console.log('path: ', req.path);
-    console.log('method: ', req.method);
-    /**There is an issue with this middleware. When we visit the page, we dont really get a response on the browser and it just keeps showing the loading icon.
-     * This is because the .use() method is executed for all request URLs, and since node reads and tries to match the handler functions from top to bottom, when it finds the first match it stops matching the handler functions
-     * This is why our request never gets matched with the appropriate app.get() method.
-     * This is also the reason why if we put the method at the bottom of this file we will never run it.
-     * 
-     * We can solve the first issue by invoking the next() method at the end of this handler method. This method just tells node to continue matching the request with the middleware(handler functions) that come after this method.
-     */
-    next();
-});
+//USING THIRD-PARTY MIDDLEWARE
+/**In this case we will use a third party middleware called morgan. It used for logging
+ * STEP 1: in the terminal run npm install morgan
+ * STEP 2: import it into this file
+ * STEP 3: invoke it in this file
+ */
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     
@@ -31,7 +23,6 @@ app.get('/', (req, res) => {
         {title: "Paste some Gibberish here", snippet: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, praesentium!"}
     ]
     res.render('index', {title : "Blog", blogs});
-    // remember that in an object, "blogs: blogs" is equal to just "blogs" 
 });
 
 app.get('/about', (req, res) => {
