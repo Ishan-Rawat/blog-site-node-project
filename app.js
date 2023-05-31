6c1364f68f53ee2be7dd317cb6967ad0005d8fe0
Ishan-Rawat <rawatishan03@gmail.com>
2023-05-31 21:36:53 +0530

    what is middleware + custom middleware

--- app.js
+++ app.js
@@ -5,9 +5,24 @@ const app = express();
 app.listen(3000); 
 
 app.set('view engine', 'ejs');
-/**This time, we want to send an array of blogs(they're all objects themselves) to the index.ejs page
- * Where, we will use a for loop to to "print" each of them out.
-*/
+
+//Creating our own custom middleware function
+//The following middleware function just logs out some of the details about the request we have received
+app.use((req, res, next) => {
+    console.log('new request made:');
+    console.log('host: ', req.hostname);
+    console.log('path: ', req.path);
+    console.log('method: ', req.method);
+    /**There is an issue with this middleware. When we visit the page, we dont really get a response on the browser and it just keeps showing the loading icon.
+     * This is because the .use() method is executed for all request URLs, and since node reads and tries to match the handler functions from top to bottom, when it finds the first match it stops matching the handler functions
+     * This is why our request never gets matched with the appropriate app.get() method.
+     * This is also the reason why if we put the method at the bottom of this file we will never run it.
+     * 
+     * We can solve the first issue by invoking the next() method at the end of this handler method. This method just tells node to continue matching the request with the middleware(handler functions) that come after this method.
+     */
+    next();
+});
+
 app.get('/', (req, res) => {
     
     const blogs = [
