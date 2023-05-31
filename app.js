db78ea10119d35e81bd3ccc10cac6ec67a146710
Ishan-Rawat <rawatishan03@gmail.com>
2023-05-31 16:39:54 +0530

    dynamic content part 2

--- app.js
+++ app.js
@@ -5,16 +5,18 @@ const app = express();
 app.listen(3000); 
 
 app.set('view engine', 'ejs');
-
-//Now lets try sending data from the handler functions to the templates
-
-//The following code allows us to send the title name of the pages from the different handler functions 
+/**This time, we want to send an array of blogs(they're all objects themselves) to the index.ejs page
+ * Where, we will use a for loop to to "print" each of them out.
+*/
 app.get('/', (req, res) => {
-    res.render('index', {title : "Blog"})
-    /**To send data from the handler function we specify an object as the second argument to the .render() method
-     * The properties can be named whatever we want
-     * The properties can be accessed directly from anywhere in the template file that is being rendered.
-     * That too without referencing the property.*/; 
+    
+    const blogs = [
+        {title: "Paste some Gibberish here", snippet: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, praesentium!"},
+        {title: "Paste some Gibberish here", snippet: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, praesentium!"},
+        {title: "Paste some Gibberish here", snippet: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, praesentium!"}
+    ]
+    res.render('index', {title : "Blog", blogs});
+    // remember that in an object, "blogs: blogs" is equal to just "blogs" 
 });
 
 app.get('/about', (req, res) => {
