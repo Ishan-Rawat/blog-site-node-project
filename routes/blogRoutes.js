a5c555f02d568f4b04657a703d3e0380c9a84027
Ishan-Rawat <rawatishan03@gmail.com>
2023-06-05 14:47:33 +0530

    created controller for all blog requests

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,53 +1,17 @@
 const express = require('express');
-const Blog = require('../models/blog');
-// Notice that we have changed the path from ./models/blog to ../models/blog in the 1st commit of this branch
-//This is because we have to get out of this folder then go into the models folder
 const router = express.Router();
-//next we need to replace app.get etc with router.get and so on...
+const  blogController = require('../controllers/blogController');
 
-/**After scoping the handler in app.js, we will have to change the URLS in the routes below
- * This is because we are already coming to these routes from /blogs, so a /blogs route in this file
- * will be applied when request URL= /blogs/blogs
- * since no one in their right mind would request that, we will change /blogs in this file to just '/' */
-router.get('/create', (req, res) => {
-    res.render('create', {title : "Create a Blog"});
-});
+router.get('/create', blogController.blog_create_get);
 
 //blog routes
-router.get("/", (req, res)=> {
-    Blog.find().sort({createdAt: -1})
-        .then((result) => {
-            res.render('index', {title: 'all blogs', blogs: result})
-        })
-        .catch((err) => console.log(err))
-});
-
+router.get("/", blogController.blog_index);
 //handling the post request from the create a blog form
-router.post('/', (req, res) =>{
-    const blog = new Blog(req.body);
-    blog.save()
-        .then((result) => {
-            res.redirect('/blogs');
-        })
-        .catch((err) => console.log(err))
-});
-router.get('/:id', (req, res) => {
-    const id = req.params.id; 
-    
-    Blog.findById(id)
-        .then((result) => {
-            res.render('details', {blog: result, title: 'blog details'})
-        })
-        .catch(err => console.log(err))
-});
-router.delete('/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findByIdAndDelete(id) //The method does what it says, and its an async method, so...
-        .then(result => {
-            res.json({ redirect: '/blogs'});
-        })
-        .catch(err => console.log(err))
-});
+router.post('/', blogController.blog_create_post);
+//request to get an individual blog
+router.get('/:id', blogController.blog_details);
+//request to delete a blog
+router.delete('/:id', blogController.blog_delete);
 
 module.exports = router;
 //next we need to import this in app.js
